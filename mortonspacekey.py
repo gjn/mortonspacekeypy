329a05bb3d2294e6245ac3adf365be57b62ffa47
gjn <gjn@space.ch>
2013-05-23 19:22:16 +0200

    Adding second algorithm which takes multiple points as input and gives enclosing morton space key

--- mortonspacekey.py
+++ mortonspacekey.py
@@ -4,6 +4,11 @@
 import unittest
 import math
 
+class Point:
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
 class BBox:
     def __init__(self, minx, miny, maxx, maxy):
         self.minx = float(minx)
@@ -57,7 +62,8 @@ class QuadTree:
         #assuming quadtree contains box which is a square
         return self.bbox.width() / math.pow(2, self.levels)
 
-    #creates the morton key space for a point inside the bbox
+    #creates the morton key space for a point
+    #returns empty string if point is not inside outer limits
     def xy_to_morton(self, x, y):
         res = '0'
         if not self.bbox.contains(x, y):
@@ -70,6 +76,31 @@ class QuadTree:
                     curQuads = curQuads[j].create_quads()
                     break
         return res
+
+    #takes array of points and returns morton space key which contains _all_ points
+    def points_to_morton(self, points):
+        def contains_all_points(bbox, points):
+            for point in points:
+                if not bbox.contains(point.x, point.y):
+                    return False
+            return True
+
+        res = '0'
+        if not contains_all_points(self.bbox, points):
+            return ''
+
+        curQuads = self.bbox.create_quads()
+        for i in range(1, self.levels):
+            has_quad = False
+            for j in range(4):
+                if contains_all_points(curQuads[j], points):
+                    has_quad = True
+                    res += str(j)
+                    curQuads = curQuads[j].create_quads()
+            if not has_quad:
+                return res
+        return res
+        
 
 
 
@@ -107,7 +138,8 @@ class TestQuadTree(unittest.TestCase):
     def test_resolution(self):
         self.assertEqual(0.45, math.floor(_gqt.resolution()*100)/100)
 
-    def test_xy_to_morton(self):
+    #1.4ms per call
+    def test_single_algorithm(self):
         self.assertEqual('', _gqt.xy_to_morton(600000,899999))
         teststring = '0' * _gqt.levels
         self.assertEqual(teststring, _gqt.xy_to_morton(420000.3, 509999.7))
@@ -117,7 +149,22 @@ class TestQuadTree(unittest.TestCase):
         self.assertEqual(teststring, _gqt.xy_to_morton(420000.3, 30000.4))
         teststring = '0' + ('3' * (_gqt.levels-1))
         self.assertEqual(teststring, _gqt.xy_to_morton(899999.8, 30000.4))
-        
+
+    #1ms per call
+    def test_multialgorithm(self):
+        self.assertEqual('', _gqt.points_to_morton([Point(600000, 899999), Point(420000.3, 509999.7)]))
+        self.assertEqual('0', _gqt.points_to_morton([Point(420000, 30000), Point(900000, 510000)]))
+        self.assertEqual('0', _gqt.points_to_morton([Point(659999.8, 269999.8), Point(660000.1, 270000.1)]))
+        self.assertEqual('012222222222222222222', _gqt.points_to_morton([Point(660000.2, 270000.2), Point(660000.1, 270000.1)]))
+        self.assertEqual('01222222222222222222', _gqt.points_to_morton([Point(660000.2, 270000.2), Point(660000.6, 270000.6)]))
+ 
+    def test_compare_single_to_multi_algorithm(self):
+        self.assertEqual(_gqt.xy_to_morton(600000,899999), _gqt.points_to_morton([Point(600000,899999)]))
+        self.assertEqual(_gqt.xy_to_morton(420000.3, 509999.7), _gqt.points_to_morton([Point(420000.3, 509999.7)]))
+        self.assertEqual(_gqt.xy_to_morton(899999.8, 509999.9), _gqt.points_to_morton([Point(899999.8, 509999.9)]))
+        self.assertEqual(_gqt.xy_to_morton(420000.3, 30000.4), _gqt.points_to_morton([Point(420000.3, 30000.4)]))
+        self.assertEqual(_gqt.xy_to_morton(899999.8, 30000.4), _gqt.points_to_morton([Point(899999.8, 30000.4)]))
+
 
 if __name__ == "__main__":
     unittest.main()
